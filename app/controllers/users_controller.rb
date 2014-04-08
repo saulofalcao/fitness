bf2527d5dc96d75b88705ca95a6fdf3b67c61126
Saulo <sfalcao@sfalcao-OptiPlex-790.(none)>
2014-04-08 16:10:44 -0300

    Adicionado metodo create, com params requeridos (user_params)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,21 @@ class UsersController < ApplicationController
 	def new
 		@user=User.new()
 	end
+
+	def create
+		@user=User.new(user_params)
+		if @user.save
+			#Colocar o que fazer com um save de sucesso
+		else
+			render 'new'
+		end
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:nome, :email, :password, :password_confirmation)
+	end
+
 end
 
 
