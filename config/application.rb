5180b0ac305a6e604a844ff3e05513a808ee564b
Saulo Falcao <saulofalcao@gmail.com>
2014-04-09 16:31:01 -0300

    Convertido lingua para pt-BR, e adicionados erros quando o signup estiver com dados inválidos

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module ControleDeEstoque
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = 'pt-BR'
     config.assets.precompile += %w(*.png *.jpg *.gif)
   end
 end
